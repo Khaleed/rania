440a8948ba51e1dcfa3328c9c6802bd06bc1c2d4
Khalid <khalid@alum.recurse.com>
2018-01-21 21:47:51 +0000

    Add environment object and pass tests

---
+++ src/environment.js
@@ -0,0 +1,109 @@
+/** Copyright (c) 2018-present, github/Khaleed.
+ *
+ * This source code is licensed under the Apache licence found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/**
+ * Environment object maps symbols to their values.
+ * This environment can be extended using user-defined variables
+ * by using `(define symbol value)`
+ **/
+
+// environment object that represents global scope
+const env = Object.create(null); // env initially has no properties
+
+// nil :: Object -> Object
+const nil = env => {
+    env["nil"] = null;
+    return env;
+};
+
+// bool :: Object -> Object
+const bool = env => {
+    env["true"] = true;
+    env["false"] = false;
+    return env;
+};
+
+// print :: Object -> Object
+const print = env => {
+    env["print"] = val => {
+        console.log(val);
+        return val;
+    };
+    return env;
+};
+
+// arithmetic :: Object -> Object
+const arithmetic = env => {
+    const operators = ["+", "-", "*", "/"];
+    return operators.reduce((acc, op) => {
+        env[op] = new Function("a, b", "return a " + op + " b;");
+        return acc;
+    }, env);
+};
+
+// logic :: Object -> Object
+const logic = env => {
+    env["and"] = (x, y) => x && y;
+    env["or"] = (x, y) => x || y;
+    env["not"] = x => !x;
+    return env;
+};
+
+// comparison :: Object -> Object
+const comparison = env => {
+    env["="] = (x, y) => (x === y ? true : false);
+    env["not="] = (x, y) => (x !== y ? true : false);
+    env[">"] = (x, y) => x > y;
+    env["<"] = (x, y) => x < y;
+    env[">="] = (x, y) => x >= y;
+    env["<="] = (x, y) => x <= y;
+    return env;
+};
+
+// first :: Object -> Object
+const first = env => {
+    env["first"] = xs => xs[0];
+    return env;
+};
+
+// last :: Object -> Object
+const last = env => {
+    env["last"] = xs => xs[xs.length - 1];
+    return env;
+};
+
+// rest :: Object -> Object
+const rest = env => {
+    env["rest"] = xs => xs.slice(1, xs.length);
+    return env;
+};
+
+const standardEnv = env => {
+    nil(env);
+    bool(env);
+    print(env);
+    arithmetic(env);
+    logic(env);
+    comparison(env);
+    first(env);
+    last(env);
+    rest(env);
+    return env;
+};
+
+export {
+    env,
+    bool,
+    nil,
+    print,
+    arithmetic,
+    logic,
+    comparison,
+    first,
+    last,
+    rest,
+    standardEnv
+};
