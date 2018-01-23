a8478f80df8cc667b8f724f1674109c3c3848225
Khalid <khalid@alum.recurse.com>
2018-01-23 15:45:35 +0000

    Scribble tests for evaluating atomic expressions

---
+++ src/__tests__/evaluate.test.js
@@ -0,0 +1,27 @@
+import { evaluate } from "../evaluate";
+import { parse } from "../rania";
+import { env, standardEnv } from "../environment";
+
+// step 3.0 - evaluate the syntax tree and compute a value for a given environment
+describe("evaluate", () => {
+    const environment = standardEnv(env);
+    describe("atomic expressions", () => {
+        it("computes a value for a number", () => {
+            const exp = parse("1");
+            expect(evaluate(exp, environment)).toEqual(1);
+        });
+        it("computes a value for a string", () => {
+            const exp = parse('"hello"');
+            expect(evaluate(exp, environment)).toEqual("hello");
+        });
+        it("returns a value for a variable in the environment", () => {
+            const exp = parse("+");
+            const add = evaluate(exp, environment);
+            expect(add(2, 2)).toEqual(4);
+        });
+        it("returns undefined for a variable that is not in the environment", () => {
+            const exp = parse("map");
+            expect(evaluate(exp, environment)).toBeUndefined();
+        });
+    });
+});
