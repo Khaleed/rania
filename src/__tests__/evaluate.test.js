3e645f0595d9cef6285e5ca569ac62b7f87b8d46
Khalid <khalid@alum.recurse.com>
2018-08-26 20:58:44 +0100

    Modify and add tests for evaluate module

--- src/__tests__/evaluate.test.js
+++ src/__tests__/evaluate.test.js
@@ -14,18 +14,27 @@ describe("evaluate", () => {
     const environment = standardEnv(env);
 
     describe("atomic expressions", () => {
-        it("computes a value for a number", () => {
+        it("returns a value for a number", () => {
             const exp = parse("1");
             expect(evaluate(exp, environment)).toEqual(1);
         });
-        it("computes a value for a string", () => {
+        it("returns a value for a string", () => {
             const exp = parse('"hello"');
             expect(evaluate(exp, environment)).toEqual("hello");
         });
+        it("returns a value for a boolean", () => {
+            const exp = parse("true");
+            expect(evaluate(exp, environment)).toEqual(true);
+        });
+
         it("returns a value for a variable in the environment", () => {
             const exp = parse("+");
             const add = evaluate(exp, environment);
             expect(add(2, 2)).toEqual(4);
+        });
+        it("returns a value for a variable in the environment", () => {
+            const exp = parse("nil");
+            expect(evaluate(exp, environment)).toEqual(null);
         });
         it("returns undefined for a variable that is not in the environment", () => {
             const exp = parse("map");
@@ -40,23 +49,45 @@ describe("evaluate", () => {
         });
         it("returns a list with a number", () => {
             const exp = parse("(1)");
-            expect(evaluate(exp, environment)).toEqual([1]);
+            expect(() => {
+                evaluate(exp, environment);
+            }).toThrow();
         });
-        it("returns a list with numbers", () => {
-            const exp = parse("(2 4 6)");
-            expect(evaluate(exp, environment)).toEqual([2, 4, 6]);
-        });
-        it("returns a list with a string", () => {
-            const exp = parse('("hi")');
-            expect(evaluate(exp, environment)).toEqual(["hi"]);
-        });
-        it("returns a list with several strings", () => {
-            const exp = parse('("yo!" "what" "up")');
-            expect(evaluate(exp, environment)).toEqual(["yo!", "what", "up"]);
-        });
+    });
+
+    describe("procedure calls", () => {
         it("returns a number for an expression with arithmetic operator", () => {
             const exp = parse("(+ 2 2)");
             expect(evaluate(exp, environment)).toEqual(4);
+        });
+        it("returns a boolean for an expression with logical operator", () => {
+            const exp = parse("(not 2)");
+            expect(evaluate(exp, environment)).toEqual(false);
+        });
+        it("returns numbers in a list except the first", () => {
+            const exp = parse("(rest 8 9 10)");
+            expect(evaluate(exp, environment)).toEqual([9, 10]);
+        });
+        it("returns first number in a list", () => {
+            const exp = parse("(first 8 9 10)");
+            expect(evaluate(exp, environment)).toEqual(8);
+        });
+        it("returns last number in a list", () => {
+            const exp = parse("(last 8 9 10)");
+            expect(evaluate(exp, environment)).toEqual(10);
+        });
+    });
+
+    describe("special", () => {
+        describe("if", () => {
+            it.only("takes question-expression then either evaluates second or third argument", () => {
+                const exp = parse("(if 2 100 0)");
+                expect(evaluate(exp, environment)).toEqual(100);
+            });
+            // it("takes question-expression then either evaluates second or third argument", () => {
+            //     const exp = parse("(if (> 4 2) 100 0)");
+            //     expect(evaluate(exp, environment)).toEqual(100);
+            // });
         });
     });
 });
