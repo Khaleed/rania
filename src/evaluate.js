5fdabc713250b2ee1d4108e8cc60e4af0263c216
Khalid <khalid@alum.recurse.com>
2018-09-04 22:36:56 +0100

    Fix typo and type signatures and refactor listEval for all expressions

--- src/evaluate.js
+++ src/evaluate.js
@@ -1,50 +1,79 @@
 /**
- * Copyright (c) 2018-present, github/Khaleed.
+ * Copyright © 2018-present, github/Khaleed.
  *
  * This source code is licensed under the Apache licence found in the
  * LICENSE file in the root directory of this source tree.
  */
 
-// symbolEval :: (Object, Object) -> Any
-const symbolEval = (exp, environment) => {
-    return exp.type === "symbol"
+import { rest, map, compose } from "./helpers/index";
+import { special } from "./special";
+
+/**
+ * symbolEval :: (Object, Object) -> Any
+ *
+ * Produces a value from symbols given an environment
+ */
+const symbolEval = (exp, environment) =>
+    exp.type === "symbol"
         ? environment[exp.value]
         : new ReferenceError(`Undefined variable ${exp.type}`);
-};
 
-// literalEval :: (Object, Object) -> Any
+/**
+ * literalEval :: (Object, Object) -> Any
+ *
+ * Produces a value from symbols given an environment
+ */
 const literalEval = (exp, environment) => {
+    console.log(exp);
     return exp.type === "number" || exp.type === "string"
         ? exp.value
         : symbolEval(exp, environment);
 };
 
-// listEval :: [Object] -> Any
+/**
+ * listEval :: [Object] -> Any
+ *
+ * Computes a value from list expressions
+ */
 const listEval = (exp, environment) => {
-    // check that the list is empty
-    if (exp.length === 0) {
-        // return the empty list
+    if (exp.length < 1) {
         return exp;
     }
-    // check if the list only has atomic expressions
-    if (exp[0].length === undefined) {
-        // return an array of the values
-        return exp.reduce((acc, x) => acc.concat(x.value), []);
+    if (exp[0].length === undefined && exp[0].type !== "symbol") {
+        // return an array containing literal values
+        throw TypeError(`${exp[0].value} is not a function`);
     }
-    return null;
+    if (exp[0].type === "symbol" && !(exp[0].value in special)) {
+        // check if symbol points to a procedure
+        const procedure = evaluate(exp[0], environment);
+        if (typeof procedure !== "function") {
+            throw TypeError(`incorrectly calling ${procedure} as a function`);
+        } else {
+            const evalArg = x => evaluate(x, environment);
+            const evalAllArgs = compose(map(evalArg), rest);
+            const args = evalAllArgs(exp);
+            return exp[0].value === "rest" ||
+                exp[0].value === "first" ||
+                exp[0].value === "last"
+                ? procedure(args)
+                : procedure(...args);
+        }
+    } else {
+        // it is a special form
+        const op = special[exp[0].value](rest(exp), environment);
+        return op;
+    }
 };
 
 /**
+ * evaluate :: ((Either [Object], Object), Object) -> Any
+ *
  * Evaluates expression by returning a value from a syntax tree and a given
  * environment that maps variables to their values.
- *
  */
-
-// evaluate :: ((Either [Object], Object), Object) -> Any
 const evaluate = (exp, environment) =>
-    // [Object] -> Any
     Array.isArray(exp) === true
         ? listEval(exp, environment)
-        : literalEval(exp, environment); // Object -> Any
+        : literalEval(exp, environment);
 
 export { evaluate };
